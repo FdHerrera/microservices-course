182f646ddea8d24fe34bad5f714edc2b5a56ea22
FdHerrera <soyfdherrera@gmail.com>
2023-02-08 10:58:04 -0300

    Fully functional rabbit queue with registration in eureka

--- rabbit-queue/src/main/java/com/fdherrera/config/AdditionalBeansConfig.java
+++ rabbit-queue/src/main/java/com/fdherrera/config/AdditionalBeansConfig.java
@@ -3,9 +3,11 @@ package com.fdherrera.config;
 import com.fdherrera.constants.QueueConstants;
 
 import org.springframework.amqp.core.Queue;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class AdditionalBeansConfig {
@@ -14,6 +16,12 @@ public class AdditionalBeansConfig {
 	public Queue queue(Environment env) {
 		String notificationsQueueName = env.getProperty(QueueConstants.NOTIFICATIONS_QUEUE_NAME);
 		return new Queue(String.valueOf(notificationsQueueName), true);
+	}
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }
--- rabbit-queue/src/main/java/com/fdherrera/rabbitmq/QueueConsumer.java
+++ rabbit-queue/src/main/java/com/fdherrera/rabbitmq/QueueConsumer.java
@@ -2,22 +2,51 @@ package com.fdherrera.rabbitmq;
 
 import java.util.logging.Logger;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fdherrera.constants.QueueConstants;
+import com.fdherrera.dto.NotificationRequest;
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 @Component
 public class QueueConsumer {
 	private static final Logger log = Logger.getLogger(QueueConsumer.class.getName());
 
+	private final RestTemplate restTemplate;
+	private final ObjectMapper mapper;
+
+	public QueueConsumer(RestTemplate restTemplate, ObjectMapper mapper) {
+		this.restTemplate = restTemplate;
+		this.mapper = mapper;
+	}
+
 	@RabbitListener(queues = { QueueConstants.NOTIFICATIONS_QUEUE_PROP })
 	public void receiveMessage(@Payload String message) {
 		log.info("Message received: " + message);
 
+		NotificationRequest notificationReceived = getNotificationRequest(message);
+
+		restTemplate.postForEntity("http://NOTIFICATION/api/v1/notification", notificationReceived, Void.class);
+
+		log.info("Message processed: " + message);
+
 		waitASec();
 
+	}
+
+	private NotificationRequest getNotificationRequest(String message) {
+		NotificationRequest notificationReceived = null;
+		try {
+			notificationReceived = mapper.readValue(message, NotificationRequest.class);
+		} catch (JsonProcessingException jpe) {
+			log.severe("Fail transforming msg into an object: " + message + " with message: " + jpe.getMessage());
+		}
+		return notificationReceived;
 	}
 
 	private void waitASec() {
